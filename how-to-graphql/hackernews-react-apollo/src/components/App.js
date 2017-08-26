86e46f85bc6210728d09bf2276d45e2b813a95a4
baurine <2008.hbl@gmail.com>
2017-08-26 22:57:56 +0800

    Prepare Login component

--- how-to-graphql/hackernews-react-apollo/src/components/App.js
+++ how-to-graphql/hackernews-react-apollo/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Header from './Header'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
+import Login from './Login'
 import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
@@ -12,8 +13,9 @@ class App extends Component {
         <Header/>
         <div className='ph3 pv1 background-gray'>
           <Switch>
-            <Route exact path='/' component={LinkList}/>
+            <Route exact path='/login' component={Login}/>
             <Route exact path='/create' component={CreateLink}/>
+            <Route exact path='/' component={LinkList}/>
           </Switch>
         </div>
       </div>

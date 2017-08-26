d5b95bbfce1710fc681f336d22d44c8c3ba1739e
baurine <2008.hbl@gmail.com>
2017-08-26 21:09:50 +0800

    Query with apollo client

--- how-to-graphql/hackernews-react-apollo/src/components/LinkList.js
+++ how-to-graphql/hackernews-react-apollo/src/components/LinkList.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
+import { graphql, gql } from 'react-apollo'
 import Link from './Link'
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [{
-      id: '1',
-      description: 'The Coolest GraphQL Backend 😎',
-      url: 'https://www.graph.cool'
-    }, {
-      id: '2',
-      description: 'The Best GraphQL Client',
-      url: 'http://dev.apollodata.com/'
-    }]
+    const { allLinksQuery } = this.props
 
+    if (allLinksQuery && allLinksQuery.loading) {
+      return <div>Loading...</div>
+    }
+    if (allLinksQuery && allLinksQuery.error) {
+      return <div>Error</div>
+    }
+
+    const linksToRender = allLinksQuery.allLinks
     return (
       <div>
         {linksToRender.map(link => (
@@ -23,4 +24,17 @@ class LinkList extends Component {
   }
 }
 
-export default LinkList
+const ALL_LINKS_QUERY = gql`
+  # 2
+  query AllLinksQuery {
+    allLinks {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+
+// name is props key
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' })(LinkList)

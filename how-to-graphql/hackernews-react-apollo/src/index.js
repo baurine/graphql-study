a93477d98dc0070f815377ade3f0386803fd21b6
baurine <2008.hbl@gmail.com>
2017-08-26 23:29:41 +0800

    Add token for every GraphQL request

--- how-to-graphql/hackernews-react-apollo/src/index.js
+++ how-to-graphql/hackernews-react-apollo/src/index.js
@@ -5,10 +5,22 @@ import registerServiceWorker from './registerServiceWorker'
 import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+import { GC_AUTH_TOKEN } from './constants'
 
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj6t4ldq40qde018487586tqg'
 })
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {}
+    }
+    const token = localStorage.getItem(GC_AUTH_TOKEN)
+    req.options.headers.authorization = token ? `Bearer ${token}` : null
+    next()
+  }
+}])
 
 const client = new ApolloClient({
   networkInterface

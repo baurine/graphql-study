7bf9d231ea75f372db9e15876e0980be5e3576f3
baurine <2008.hbl@gmail.com>
2017-08-27 11:35:13 +0800

    Search links for apollo filter parameter

--- how-to-graphql/hackernews-react-apollo/src/components/App.js
+++ how-to-graphql/hackernews-react-apollo/src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 import Login from './Login'
+import Search from './Search'
 import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
         <Header/>
         <div className='ph3 pv1 background-gray'>
           <Switch>
+            <Route exact path='/search' component={Search}/>
             <Route exact path='/login' component={Login}/>
             <Route exact path='/create' component={CreateLink}/>
             <Route exact path='/' component={LinkList}/>

d1d1e2778f24a688314fae6e3d610f13aad5967f
baurine <2008.hbl@gmail.com>
2017-08-27 12:10:04 +0800

    Subscribe new link and new vote

--- how-to-graphql/hackernews-react-apollo/src/index.js
+++ how-to-graphql/hackernews-react-apollo/src/index.js
@@ -5,11 +5,25 @@ import registerServiceWorker from './registerServiceWorker'
 import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 import { GC_AUTH_TOKEN } from './constants'
 
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj6t4ldq40qde018487586tqg'
 })
+
+const wsClient = new SubscriptionClient(
+  'wss://subscriptions.ap-northeast-1.graph.cool/v1/cj6t4ldq40qde018487586tqg', {
+  reconnect: true,
+  connectionParams: {
+    authToken: localStorage.getItem(GC_AUTH_TOKEN),
+  }
+})
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+)
 
 networkInterface.use([{
   applyMiddleware(req, next) {
@@ -23,7 +37,7 @@ networkInterface.use([{
 }])
 
 const client = new ApolloClient({
-  networkInterface
+  networkInterface: networkInterfaceWithSubscriptions
 })
 
 ReactDOM.render(

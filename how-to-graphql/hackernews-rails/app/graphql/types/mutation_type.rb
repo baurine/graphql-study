99aece47df4d8d2a09fd042eb1a1d0286ebcd880
baurine <2008.hbl@gmail.com>
2017-09-02 19:58:43 +0800

    First query - allLinks

--- how-to-graphql/hackernews-rails/app/graphql/types/mutation_type.rb
+++ how-to-graphql/hackernews-rails/app/graphql/types/mutation_type.rb
@@ -2,4 +2,7 @@ Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
   # TODO: Add Mutations as fields
+  field :allLinks, !types[Types::LinkType] do
+    resolve -> (obj, args, ctx) { Link.all }
+  end
 end

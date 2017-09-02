93022769f7a22096c44b5a901f590aa4dc70610e
baurine <2008.hbl@gmail.com>
2017-09-02 20:56:08 +0800

    Generate User model

--- how-to-graphql/hackernews-rails/db/schema.rb
+++ how-to-graphql/hackernews-rails/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170902082323) do
+ActiveRecord::Schema.define(version: 20170902124349) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
